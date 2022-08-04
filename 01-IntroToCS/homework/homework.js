53f4687965424dceed9b27192729ba33e7a80422
Alisser2001 <105648476+Alisser2001@users.noreply.github.com>
2022-08-03 20:08:21 -0500

    01-IntroToCS completed

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,30 +2,12 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  let dec = 0;
-  let arr = num.toString().split("");
-  let myStr = arr.reverse().join("");
-  for (let i=0; i<=myStr.length - 1; i++) {
-    let bin = parseInt(myStr[i]);
-    dec += ((2**i)*bin);
-  }
-  return dec;
+  return parseInt(num, 2);
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-  let myArr = [];
-  let residuo = num % 2;
-  let coc = Math.trunc(num / 2);
-  myArr.push(residuo);
-  while(coc >= 2) {
-    residuo = coc % 2;
-    coc = Math.trunc(coc / 2);
-    myArr.push(residuo);
-  }
-  myArr.push(coc);
-  let bin = myArr.reverse().join("");
-  return bin;
+  return num.toString([2]);
 }
 
 
