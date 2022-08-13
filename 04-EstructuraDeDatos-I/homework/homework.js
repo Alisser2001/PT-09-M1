9b0c5c7ff1f0d9ee87de7227a0ad6ab354b187df
Alisser2001 <105648476+Alisser2001@users.noreply.github.com>
2022-08-13 10:40:34 -0500

    Estructuras de Datos I completed

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -3,9 +3,13 @@
 /*
 Definir las funciones recursivas nFactorial y nFibonacci.
 
-nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial (representado como n!) es el producto de n por todos los números naturales menores que él y mayores a 0. Ejemplo: 5! = 5 * 4 * 3 * 2 * 1
+nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial 
+(representado como n!) es el producto de n por todos los números naturales menores que él y mayores a 0. 
+Ejemplo: 5! = 5 * 4 * 3 * 2 * 1
 
-nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
+nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, 
+respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que 
+se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
 Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
 
 Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
@@ -15,13 +19,32 @@ Como ejercicio adicional y completamente opcional, al terminar de resolver este 
 */
 
 function nFactorial(n) {
+  if(n>-1 && n<2) {
+    return 1;
+  }
+  return n*nFactorial(n-1)
 }
 
 function nFibonacci(n) {
+  if(n<=1) {
+    return n
+  } else {
+    return nFibonacci(n-1) + nFibonacci(n-2) //Aunque se usa recursividad, es más pesado para el sistema
+  }
+  /*Sin recursividad: Más óptimo
+  let arrFib = [];
+  for (let i=0; i<=n; i++) {
+    arrFib.push(i);
+  }
+  for (let i=2; i<=n; i++) {
+    arrFib[i] = arrFib[i-1] + arrFib[i-2];
+  }
+  return arrFib[n];*/
 }
 
 /*
-Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
+Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, 
+donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
   - enqueue: agrega un valor respetando el orden.
   - dequeue: remueve un valor respetando el orden. Retorna undefined cuando la queue está vacía.
   - size: retorna el tamaño (cantidad de elementos) de la queue.
@@ -30,7 +53,16 @@ Pueden utilizar class o función constructora.
 */
 
 function Queue() {
-
+   this.arr = []
+}
+Queue.prototype.enqueue = function(value) {
+  this.arr.push(value);
+}
+Queue.prototype.dequeue = function() {
+  return this.arr.shift();
+}
+Queue.prototype.size = function() {
+  return this.arr.length;
 }
 
 // No modifiquen nada debajo de esta linea
