fd60b62ab590cd5a03fe0691acf7f90b78c35f04
Alisser2001 <105648476+Alisser2001@users.noreply.github.com>
2022-08-20 13:20:06 -0500

    06-EstructuraDeDatos-III completed

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -18,12 +18,13 @@ function BinarySearchTree(value) {
 }
 
 BinarySearchTree.prototype.size = function() {
-  let cont = 1
-  if(this.left) {
+  let cont = 1 //Iniciamos el contador en cero haciendo referencia al valor head del arbol
+  if(this.left) { //Si exite un valor a la izquierda, aumentamos el contador en 1
     cont += 1
-    this.left.size()
+    this.left.size() /*Con ayuda de la recursión, la función se ejecutará hacia ambos lados siempre
+    y cuendo existen valores*/
   } 
-  if (this.right) {
+  if (this.right) { //Si existe un valor a la derecha, aumentamos el contador en 1
     cont += 1
     this.right.size()
   }
@@ -31,32 +32,79 @@ BinarySearchTree.prototype.size = function() {
 }
 
 BinarySearchTree.prototype.insert = function(value) {
-  if (value<this.value && this.left===null) {
+  if (value<this.value && this.left===null) { /*Si el valor ingresado es menor que el head va a la izquiera
+  Y si el nodo izquierdo no tiene ningún valor asignado, se crea uno y se le asigna el valor ingresado*/
     this.left = new BinarySearchTree(value)
-  } else if (value>=this.value && this.right===null) {
+  } else if (value>=this.value && this.right===null) {/*Si el valor ingresado es mayor que el head va a la derecha
+  Y si el nodo derecho no tiene ningún valor asignado, se crea uno y se le asigna el valor ingresado*/
     this.right = new BinarySearchTree(value)
-  } else if(value<this.value && this.left!==null) {
+  } else if(value<this.value && this.left!==null) {/*Si es menor que el head y el nodo izquiero está ocupado
+  Se usa recursión para ejecutar la funcón hasta que se encuentre un espacio donde alguno de los primeros
+  condicionales if pueda crear un nodo*/
     this.left.insert(value)
-  } else if (value>=this.value && this.right!==null) {
+  } else if (value>=this.value && this.right!==null) {/*Si es mayor que el head y el nodo derecho está ocupado
+  Se usa recursión para ejecutar la función hasta que se encuentre un espacio donde alguno de los primeros
+  condicionales if pueda crear un nodo*/
     this.right.insert(value)
   }
 }
 BinarySearchTree.prototype.contains = function(value) {
-  if(this.value === value) {
+  if(this.value === value) { //Si el valor es igual al value del nodo actual, se retorna true
     return true 
-  } else if(value<=this.value && this.left !== null) {
+  } else if(value<=this.value && this.left !== null) { /*Si no es igual y es menor que el nodo head,
+  se usa recursión para ejecutar la función hasta que el primer if retorne true*/ 
     return this.left.contains(value)
-  } else if(value>this.value && this.right !== null) {
+  } else if(value>this.value && this.right !== null) { /*Si no es igual y es mayor que el nodo head,
+  se usa recursión para ejecutar la función hasta que el primer if retorne true*/ 
     return this.right.contains(value)
   } else {
-    return false
+    return false //Si ninguno retorna true al no haber más nodos por recorrer, se retorna false
   }
 }
-BinarySearchTree.prototype.depthFirstForEach = function() {
-
+BinarySearchTree.prototype.depthFirstForEach = function(fcb, order) {
+  if (order === 'pre-order') { 
+    fcb(this.value) //Evalua el primer valor
+    if(this.left !== null) { //Se ejecuta primero hacia la izquierda, devolviendo cada valor
+      this.left.depthFirstForEach(fcb, order)
+    }
+    if(this.right !== null) { /*Cuando no hay nodos a la izquierda, sigue con el de la derecha 
+    Y si tiene nodos a las izquierda, sigue por ese camino*/
+      this.right.depthFirstForEach(fcb, order)
+    }
+  } else if (order === 'post-order') { /*Se va hasta el último de la izquierda y retorna los valores 
+  de los últimos nodos primero, luego el de su padre*/
+    if (this.left !== null) {
+      this.left.depthFirstForEach(fcb, order)
+    }
+    if (this.right !== null) {
+      this.right.depthFirstForEach(fcb, order)
+    }
+    fcb(this.value)
+  } else { //in-order Opción por defecto
+    if (this.left !== null) {
+      this.left.depthFirstForEach(fcb, order)
+    }
+    fcb(this.value)
+    if (this.right !== null) {
+      this.right.depthFirstForEach(fcb, order)
+    }
+  }
 }
-BinarySearchTree.prototype.breadthFirstForEach = function() {
-
+BinarySearchTree.prototype.breadthFirstForEach = function(fcb, myArr = []) {
+  //Se crea un array como parametro para enviar los nodos
+  //Lo que hace el código es evaluar en la fcb el primer valor y enviar los nodos left y right al array
+  //Luego usa recursión para evaluar los nodos del array uno por uno, añadiendo los nodos left y right 
+  //De cada uno y pasando a la fcb el nodo evaluado
+  if(this.left !== null) {
+    myArr.push(this.left)
+  }
+  if(this.right !== null) {
+    myArr.push(this.right)
+  }
+  fcb(this.value)
+  if (myArr.length > 0) {
+    myArr.shift().breadthFirstForEach(fcb, myArr)
+  }
 }
 // No modifiquen nada debajo de esta linea
 // --------------------------------
